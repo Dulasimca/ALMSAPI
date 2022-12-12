234e8c42dec98da7a1f9332ae5690617521b009f
ananth61 <ananth.murugan16@gmail.com>
2022-12-12 17:28:19 +0530

    Product master API

---
+++ AMS/productmaster.cs
@@ -0,0 +1,41 @@
+﻿using ALMS_API.ManageSQL;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace ALMS_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class productmaster : ControllerBase
+
+    {
+        [HttpGet(nameof(Getproductmaster))]
+
+        public string Getproductmaster()
+        {
+            {
+                ManageSQLConnection manageSQL = new ManageSQLConnection();
+
+                var result = manageSQL.Getproductmaster();
+                return JsonConvert.SerializeObject(result);
+            }
+        }
+    }
+    public class productmasterEntity
+    {
+        public int productid { get; set; }
+        public string productname { get; set; }
+        public bool flag { get; set; }
+
+    }
+}
+
+
+
+
