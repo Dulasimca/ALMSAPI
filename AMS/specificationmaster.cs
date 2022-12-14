65483ac9c170d9999cb121cbcd68e2f0ceba9af7
ananth61 <ananth.murugan16@gmail.com>
2022-12-14 17:07:26 +0530

    brand and product

---
+++ AMS/specificationmaster.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Newtonsoft.Json;
+using ALMS_API.ManageSQL;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALMS_API.AMS
+{
+    public class specificationmaster : Controller
+    {
+        [HttpPost("{id}")]
+
+        public string Specificationmaster(specificationmasterEntity specificationmasterEntity)
+        {
+            try
+            {
+                ManageSQLConnection manageSQL = new ManageSQLConnection();
+                var result = manageSQL.insertspecificationmaster(specificationmasterEntity);
+                return JsonConvert.SerializeObject(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return "false";
+        }
+        [HttpGet]
+        public string Getspecificationmaster()
+        {
+            {
+                ManageSQLConnection manageSQL = new ManageSQLConnection();
+
+                var result = manageSQL.Getspecificationmaster();
+                return JsonConvert.SerializeObject(result);
+            }
+        }
+    }
+    public class specificationmasterEntity
+    {
+        public int id { get; set; }
+        public int productid { get; set; }
+        public int brandid { get; set; }
+        public string name { get; set; }
+        public string specification { get; set; }
+        public bool flag { get; set; }
+
+
+    }
+}
